b0f27d2a10d23f508832e0648f60a4e10f14719f
My comp <milafox.222@gmail.com>
2022-12-06 21:59:15 +0200

    H/W3

--- HomeWork3.py
+++ HomeWork3.py
@@ -1,27 +1,55 @@
 a = float(input('Ввести операнд 1:'))
 b = float(input('Ввести операнд 2:'))
-print(
-    '''
-    Сложение  '+'
-    Вычетание '-'
-    Деление   '/'
-    Умножение '*'
-    Возведение в степень '**'
-    '''
-)
 user_choice = input('Выбрать действие:')
+# Два варианта калькулятора - или или
 if user_choice == '+':
+    print(a + b)
     print(f'{a} + {b} = {a + b}')
 elif user_choice == '-':
+    print(a - b)
     print(f'{a} - {b} = {a - b}')
 elif user_choice == '/':
+    print(a / b)
     print(f'{a} / {b} = {a / b}')
 elif user_choice == '*':
+    print(a * b)
     print(f'{a} * {b} = {a * b}')
 elif user_choice == '**':
+    print(a ** b)
     print(f'{a} ** {b} = {a ** b}')
 else:
     print()
-
-
-
+end_number = int(input('Ввести целое число N:'))
+start_number = 1
+step = int(f'{(start_number + 1) ** 2}')
+for number in range(start_number, end_number, step):
+    print(number, end=' ')
+print()
+number = int(input('Введите число для проверки:'))
+if number % 2 != 0:
+    print('Простое чсло')
+else:
+    print('Сложное число')
+K = int(input('Введите кол-во грибов K:'))
+if K % 10 == 1:
+    print(f'Маша нашла в лесу {K} гриб')
+elif K % 10 == 2:
+    print(f'Маша нашла в лесу {K} гриба')
+elif K % 10 == 3:
+    print(f'Маша нашла в лесу {K} гриба')
+elif K % 10 == 4:
+    print(f'Маша нашла в лесу {K} гриба')
+elif K % 10 == 0:
+    print(f'Маша нашла в лесу {K} грибов')
+elif K % 10 == 5:
+    print(f'Маша нашла в лесу {K} грибов')
+elif K % 10 == 6:
+    print(f'Маша нашла в лесу {K} грибов')
+elif K % 10 == 7:
+    print(f'Маша нашла в лесу {K} грибов')
+elif K % 10 == 8:
+    print(f'Маша нашла в лесу {K} грибов')
+elif K % 10 == 9:
+    print(f'Маша нашла в лесу {K} грибов')
+else:
+    print()
