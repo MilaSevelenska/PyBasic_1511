1dd724eec810be6ca82a780c1319701a990612f3
My comp <milafox.222@gmail.com>
2022-12-07 23:57:23 +0200

    H/W3

--- Draft_Work.py
+++ Draft_Work.py
@@ -1,4 +1,8 @@
-number = int(input('Ввести целое число N:'))
-for i in range(1, number):
-    print(i ** 2, end=' ')
-print()
+
+number = int(input('Введите любое число для проверки:'))
+if number == 2 or number == 3:
+    print('Простое число')
+elif number % 2 != 0 and number % 3 != 0:
+    print('Простое число')
+else:
+    print('Сложное число')
--- HomeWork3.py
+++ HomeWork3.py
@@ -19,9 +19,15 @@ elif user_choice == '**':
     print(f'{a} ** {b} = {a ** b}')
 else:
     print()
-
-number = int(input('Введите любое число для проверки от 4 и >:'))
-if number % 2 != 0 and number % 3 != 0:
+number = int(input('Ввести целое число N:'))
+for i in range(1, number):
+  if number >= i ** 2:
+    print(i ** 2, end=' ')
+print()
+number = int(input('Введите любое число для проверки:'))
+if number == 2 or number == 3:
+    print('Простое число')
+elif number % 2 != 0 and number % 3 != 0:
     print('Простое число')
 else:
     print('Сложное число')
